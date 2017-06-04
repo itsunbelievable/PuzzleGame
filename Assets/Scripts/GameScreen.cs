2602ec41f2032f17c220141f5b324b13941bbd63
itsunbelievable <kovyazinsi@gmail.com>
2017-06-04 23:13:45 +0300

    Improved tile matching

---
+++ Assets/Scripts/GameScreen.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScreen: MonoBehaviour
+{
+    public Grid gridScript;
+    public Blocks blocksScript;
+    void Awake()
+    {
+        gridScript = GetComponent<Grid>();
+        gridScript.GridSetup();
+
+        blocksScript = GetComponent<Blocks>();
+        blocksScript.CreateBlocks(GetRandom());
+
+        gridScript.OnGridTileClicked += GridScript_OnGridTileClicked;
+    }
+
+    private void GridScript_OnGridTileClicked(object sender, System.EventArgs e)
+    {
+        Tile clickedTile = (Tile) sender;
+        clickedTile.Color = blocksScript.GetTile.Color;
+    }
+
+    void FixedUpdate ()
+    {
+        if (blocksScript.IsEmpty)
+        {
+            blocksScript.CreateBlocks(GetRandom());
+        }
+    }
+
+    int GetRandom()
+    {
+        int i = Random.Range(1, 100);
+        if (i < 10)
+            return 1;
+        if (i < 50)
+            return 2;
+        if (i < 90)
+            return 3;
+        else
+            return 4;
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Tile : MonoBehaviour
+{
+    private Material mat;
+
+    public delegate void TileClicked(object sender, EventArgs e);
+    public event TileClicked OnTileClicked;
+    public int row, column;
+    void Awake()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    public bool HasColor { get { return Color != Color.white; } }
+
+    public Color Color
+    {
+        get { return mat.color; }
+        set { mat.color = value; }
+    }
+
+    void Click(Tile reciever)
+    {
+        if (OnTileClicked != null)
+            OnTileClicked(reciever, EventArgs.Empty);
+    }
+}
+	
